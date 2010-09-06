e0deac1152e02f3689871f5061ea40109c4bd679
Nao <nao@wedge>
2010-09-06 12:06:31 +0000

    - Removed smflib folder (not used in SMF2, not likely to be used in Wedge.) - Removed compatibility code for array_combine, array_fill, sha1, file_put_contents, ob_clean, ob_get_level (although this one isn't really needed.) (create_backup.php, repair.php, restore_backup.php, smf_api.php, webinstall.php, Display.php, DumpDatabase.php) - Removed rawdata templating (leftover from SMF 1.0 days.) (ManageMaintenance.php, ManageServer.php, RepairBoards.php, Subs.php)

--- tools/webinstall.php
+++ tools/webinstall.php
@@ -216,25 +216,6 @@ function initialize_inputs()
 	if (!isset($_SESSION['webinstall_state']))
 		$_SESSION['webinstall_state'] = array();
 
-	// Create a file - this is defined in PHP 5, just use the same function name.
-	if (!function_exists('file_put_contents'))
-	{
-		function file_put_contents($filename, $data)
-		{
-			$text_filetypes = array('php', 'txt', '.js', 'css', 'vbs', 'tml', 'htm');
-
-			$fp = fopen($filename, in_array(substr($filename, -3), $text_filetypes) ? 'wt' : 'wb');
-
-			if (!$fp)
-				return 0;
-
-			fwrite($fp, $data);
-			fclose($fp);
-
-			return strlen($data);
-		}
-	}
-
 	if (isset($_GET['ftphelp']))
 	{
 		global $txt;
@@ -1343,100 +1324,6 @@ function fetch_install_info()
 	$_SESSION['webinstall_state']['install_info'] = $info;
 
 	return $info;
-}
-
-if (!function_exists('sha1'))
-{
-	function sha1($str)
-	{
-		// If we have mhash loaded in, use it instead!
-		if (function_exists('mhash') && defined('MHASH_SHA1'))
-			return bin2hex(mhash(MHASH_SHA1, $str));
-
-		$nblk = (strlen($str) + 8 >> 6) + 1;
-		$blks = array_pad(array(), $nblk * 16, 0);
-
-		for ($i = 0; $i < strlen($str); $i++)
-			$blks[$i >> 2] |= ord($str{$i}) << (24 - ($i % 4) * 8);
-
-		$blks[$i >> 2] |= 0x80 << (24 - ($i % 4) * 8);
-
-		return sha1_core($blks, strlen($str) * 8);
-	}
-
-	// This is the core SHA-1 calculation routine, used by sha1().
-	function sha1_core($x, $len)
-	{
-		@$x[$len >> 5] |= 0x80 << (24 - $len % 32);
-		$x[(($len + 64 >> 9) << 4) + 15] = $len;
-
-		$w = array();
-		$a = 1732584193;
-		$b = -271733879;
-		$c = -1732584194;
-		$d = 271733878;
-		$e = -1009589776;
-
-		for ($i = 0, $n = count($x); $i < $n; $i += 16)
-		{
-			$olda = $a;
-			$oldb = $b;
-			$oldc = $c;
-			$oldd = $d;
-			$olde = $e;
-
-			for ($j = 0; $j < 80; $j++)
-			{
-				if ($j < 16)
-					$w[$j] = @$x[$i + $j];
-				else
-					$w[$j] = sha1_rol($w[$j - 3] ^ $w[$j - 8] ^ $w[$j - 14] ^ $w[$j - 16], 1);
-
-				$t = sha1_rol($a, 5) + sha1_ft($j, $b, $c, $d) + $e + $w[$j] + sha1_kt($j);
-				$e = $d;
-				$d = $c;
-				$c = sha1_rol($b, 30);
-				$b = $a;
-				$a = $t;
-			}
-
-			$a += $olda;
-			$b += $oldb;
-			$c += $oldc;
-			$d += $oldd;
-			$e += $olde;
-		}
-
-		return dechex($a) . dechex($b) . dechex($c) . dechex($d) . dechex($e);
-	}
-
-	function sha1_ft($t, $b, $c, $d)
-	{
-		if ($t < 20)
-			return ($b & $c) | ((~$b) & $d);
-		if ($t < 40)
-			return $b ^ $c ^ $d;
-		if ($t < 60)
-			return ($b & $c) | ($b & $d) | ($c & $d);
-
-		return $b ^ $c ^ $d;
-	}
-
-	function sha1_kt($t)
-	{
-		return $t < 20 ? 1518500249 : ($t < 40 ? 1859775393 : ($t < 60 ? -1894007588 : -899497514));
-	}
-
-	function sha1_rol($num, $cnt)
-	{
-		$z = hexdec(80000000);
-		if ($z & $num)
-			$a = ($num >> 1 & (~$z | 0x40000000)) >> (31 - $cnt);
-		else
-			$a = $num >> (32 - $cnt);
-
-		return ($num << $cnt) | $a;
-	}
 }
 
 function load_language_data()
