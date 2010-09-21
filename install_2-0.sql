4e725f6ce48570ff6f51ffd6e9cbeae728960711
Arantor <sleeping@myperch.org>
2010-09-21 01:05:25 +0000

    + Added Yandex to the list of supported search engines (install_2-0.sql)

--- install_2-0.sql
+++ install_2-0.sql
@@ -1872,7 +1872,8 @@ VALUES (1, 'Google', 'googlebot', ''),
 	(16, 'InternetArchive', 'ia_archiver-web.archive.org', ''),
 	(17, 'Alexa', 'ia_archiver', ''),
 	(18, 'Omgili', 'omgilibot', ''),
-	(19, 'EntireWeb', 'Speedy Spider', '');
+	(19, 'EntireWeb', 'Speedy Spider', ''),
+	(20, 'Yandex', 'Yandex');
 
 #
 # Table structure for table `subscriptions`
