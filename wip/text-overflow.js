038566819ea61c0f4a2c1890d61dbbd48421ef7e
Nao <nao@wedge>
2011-03-13 20:26:22 +0000

    + Added a 'wip' folder (Works in progress), because we need some place to commit stuff that is either not finished but could use retooling, or finished but not actually used anywhere. (other/wip, other/wip/text-overflow.js) * Minor restyling of topic titles. (sections.css)

---
+++ wip/text-overflow.js
@@ -0,0 +1,46 @@
+
+// Based on:
+// http://devongovett.wordpress.com/2009/04/06/text-overflow-ellipsis-for-firefox-via-jquery/
+
+//	.ellipsis
+//		text-overflow: ellipsis
+//		overflow: hidden
+
+(function ($) {
+	$.fn.ellipsis = function (enableUpdating) {
+		var
+			el = $(this), text = originalText = el.html(), w = el.width(),
+			t = $(this.cloneNode(true)).hide().css({
+				position: 'absolute',
+				overflow: 'visible',
+				'max-width': 'inherit'
+			});
+		t.css(t.css('width') == 'auto' ? 'height' : 'width', 'auto');
+
+		el.after(t).attr('title', text);
+		while (text.length > 0 && t.width() > el.width())
+		{
+			text = text.substr(0, text.length - 1);
+			t.html(text + '&hell;');
+		}
+		el.html(t.html());
+		t.remove();
+
+		if (enableUpdating)
+		{
+			var oldW = el.width();
+			setInterval(function () {
+				if (el.width() != oldW)
+				{
+					oldW = el.width();
+					el.html(originalText);
+					el.ellipsis();
+				}
+			}, 1000);
+		}
+	};
+
+	var s = document.documentElement.style;
+	if (!('textOverflow' in s || 'OTextOverflow' in s))
+		$('.ellipsis').each(function () { $(this).ellipsis($(this).hasClass('update')); });
+})(jQuery);
