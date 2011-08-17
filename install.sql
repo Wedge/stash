046ab2b81036b7e828864fea05adc85a1259ecad
Arantor <sleeping@myperch.org>
2011-08-17 13:25:54 +0000

    ! Removal of the last vestiges of 'do not show contact details to guests, they're just not shown to guests now. (ManageSettings.php, Security.php, SendTopic.php, Help and ManageSettings language files) ! Custom fields now have an option for visible to guests (only when the field would otherwise be available to regular users), which means the profile fields that are used for things like AIM can be hidden from guests, which was the remaining thing controlled by the above setting. (install.sql, Load.php, ManageMemberOptions.php, Admin.template.php, ManageSettings language file)

--- install.sql
+++ install.sql
@@ -855,6 +855,7 @@ CREATE TABLE {$db_prefix}custom_fields (
   show_display tinyint(3) NOT NULL default '0',
   show_profile varchar(20) NOT NULL default 'forumprofile',
   private tinyint(3) NOT NULL default '0',
+  guest_access tinyint(3) NOT NULL default '0',
   active tinyint(3) NOT NULL default '1',
   bbc tinyint(3) NOT NULL default '0',
   can_search tinyint(3) NOT NULL default '0',
