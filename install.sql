decbc43826b814cc347d8113ffd38c4755d33dc5
Nao <nao@wedge>
2011-10-18 16:32:41 +0000

    + Wedge will now happily use the membergroup's color when showing profile links -- all the time, instead of just in the Who's Online list and info center. (Subs-Template.php, Subs.php, ManageMembers.language.php) * Changed default admin and global moderator membergroup colors to something neater that doesn't jump at you. Added a default color for the Hero group (i.e. members with over 500 posts.) (other/install.sql) - Removed group color retrieval queries from Who's Online and Info Center, since this is being handled elsewhere now. (Subs-MembersOnline.php, Who.php, Who.template.php) - Removed ability to sort online members by group color... Did anyone actually know this was possible? And what was the point, considering sorting by group name is also possible? (Subs-MembersOnline.php) - SSI's ssi_recentTopics() never made use of membergroup colors either. (SSI.php) * I like #c06002 as a color, but h4 (poster name in display template) will now be a virgin when it comes to default color. (sections.css) + Profile pages will now show the group/post-group color on the user name, even though it's not technically a profile link. (Profile.template.php)

--- install.sql
+++ install.sql
@@ -1662,14 +1662,14 @@ CREATE TABLE {$db_prefix}membergroups (
 
 INSERT INTO {$db_prefix}membergroups
 	(id_group, group_name, description, online_color, min_posts, stars, group_type)
-VALUES (1, '{$default_administrator_group}', '', '#FF0000', -1, '5#rankadmin.gif', 1),
-	(2, '{$default_global_moderator_group}', '', '#0000FF', -1, '5#rankgmod.gif', 0),
+VALUES (1, '{$default_administrator_group}', '', '#d2653a', -1, '5#rankadmin.gif', 1),
+	(2, '{$default_global_moderator_group}', '', '#c18933', -1, '5#rankgmod.gif', 0),
 	(3, '{$default_moderator_group}', '', '', -1, '5#rankmod.gif', 0),
 	(4, '{$default_newbie_group}', '', '', 0, '1#rank.gif', 0),
 	(5, '{$default_junior_group}', '', '', 50, '2#rank.gif', 0),
 	(6, '{$default_full_group}', '', '', 100, '3#rank.gif', 0),
 	(7, '{$default_senior_group}', '', '', 250, '4#rank.gif', 0),
-	(8, '{$default_hero_group}', '', '', 500, '5#rank.gif', 0);
+	(8, '{$default_hero_group}', '', '#5e8e75', 500, '5#rank.gif', 0);
 # --------------------------------------------------------
 
 #
