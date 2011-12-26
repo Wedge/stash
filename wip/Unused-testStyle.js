34627c1416ffae11ab1364c4d8d765aa78d0e8b9
Nao <nao@wedge>
2011-12-26 09:01:48 +0000

    + Added (or restored?) the ability to change options while refreshing the custom select boxes. It's just a few bytes... (sbox.js) * Set the default 'fixed' param in select boxes to 'true', because... Well, I've tried it, improved it a bit and it's better. Plus, I'm going to go through all select boxes anyway so I'll quickly spot any box that needs fixing. (sbox.js) * Did more proper calculation of maximum select box width in non-fixed mode, taking padding and margin into account. (sbox.js, index.css) * Moved the testStyle function out of the script and into the wip folder. Turns out I hadn't been using can_borderradius or can_boxshadow for quite some time. If someone wants to use them, well... I guess I can distribute the file on the forum or something. (script.js, Unused-testStyle.js)

---
+++ wip/Unused-testStyle.js
@@ -0,0 +1,15 @@
+
+function _testStyle(sty, undefined)
+{
+	var uc = sty.charAt(0).toUpperCase() + sty.substr(1), stys = [ sty, 'Moz' + uc, 'Webkit' + uc, 'Khtml' + uc, 'ms' + uc, 'O' + uc ], i;
+	for (i in stys) if (_w.style[stys[i]] !== undefined) return true;
+	return false;
+}
+
+// Has your browser got the goods?
+// These variables aren't used, but you can now use them in your custom scripts.
+// In short: if (!can_borderradius) inject_rounded_border_emulation_hack();
+var
+	_w = document.createElement('wedgerocks'),
+	can_borderradius = _testStyle('borderRadius'),
+	can_boxshadow = _testStyle('boxShadow');
