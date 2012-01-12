c650c6a32130b5e9722f271103000404512dd84b
Nao <nao@wedge>
2012-01-12 09:04:52 +0000

    + Added ability for users to add a Custom.template.php file to their theme, allowing them to override/add/prepend code to their index template without modifying it, which can be very useful if they want to customize any template in a minor way that doesn't require creating a new theme, and still be able to upgrade their copy of Wedge without re-editing the files every time. Heck, that's a long explanation for just one line of code...! (Load.php, attic/Custom.template.php) * Repositioned select box styling in the WeCSS file to help with selector specificity and crap like that. (index.css) * Just a minor if() rewrite for readability. (Subs-Template.php)

---
+++ attic/Custom.template.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+	This is just a silly example of a Custom template file, where users
+	can override, add or prepend code to an existing function. This is
+	most useful for modifying a function in the index template without
+	having to create a new theme based on the current one.
+
+	Seriously, you have no idea how much some people hate doing theme work.
+	(And yes, that includes me. -- Nao)
+*/
+
+function template_sidebar_feed_override()
+{
+	global $txt;
+
+	echo '
+		<we:title>
+			<div class="feed_icon"></div>
+			', $txt['feed'], '
+		</we:title>
+		<br>
+		<em>...SPOILERS!</em>';
+}
+
+?>
