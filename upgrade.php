9b4c7fde4ebdaac8eea2613a723f9c6ca6ed2007
Nao <nao@wedge>
2012-08-23 17:29:48 +0000

    + The Skin Selector will now provide the number of users for each skin (in addition to the global number for their theme.) That wedge_get_skin function is short and sweet, but getting the reference return to work was a PITA... If someone could look into the logic of it and spot any superfluous ampersands, that would be swell... (Themes.php, Themes.template.php) * Spacinazi. Just two bytes... For a file we'll end up removing. (upgrade.php)

--- upgrade.php
+++ upgrade.php
@@ -3235,7 +3235,7 @@ function template_upgrade_above()
 
 	if (isset($upcontext['step_progress']))
 		echo '
-				<div style="font-size: 8pt; height: 12pt; border: 1px solid black; background-color: white; width: 50%; margin: 5px auto; ">
+				<div style="font-size: 8pt; height: 12pt; border: 1px solid black; background-color: white; width: 50%; margin: 5px auto">
 					<div id="step_text" style="color: #000; position: absolute; margin-left: -5em">', $upcontext['step_progress'], '%</div>
 					<div id="step_progress" style="width: ', $upcontext['step_progress'], '%; height: 12pt; z-index: 1; background-color: #ffd000">&nbsp;</div>
 					<div class="progress">', $txt['upgrade_step_progress'], '</div>
