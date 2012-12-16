fb2f7752e220a8339961bed671c2eb7182fdedde
Nao <nao@wedge>
2012-12-16 14:43:26 +0000

    @ This is the 'revolutionary' update I'd hoped to commit as r1789, ah ah, but it wasn't ready yet.
    * Rewrote much of the detectBrowser() function. (Load.php, Home.php, Aeva-Gallery.php, Post2.php, Welcome.php, Xml.template.php, editor.js, script.js, index.css, Wuthering/skin.xml, attic/Unused-dynPrefixed.php)
    
      Wedge will no longer return 'iphone' and 'tablet' entries -- these should be addressed through media queries primarily, and/or analysis of the user agent string.
      Browser and OS are now stored in two different variables -- $context['browser']['agent'] and $context['browser']['os']
      Browser and OS versions are also stored in 'version' and 'os_version' variables. Minor updates like the '5' in '4.1.5' aren't stored, to save space.
      Likewise, old OS versions aren't stored (Windows < XP, iOS < 4, Android < 2).
    
    * Rewrote much of the hasBrowser() function as well... (Load.php, Wine/extra.css)
    
      Added support for OS names, now a test against 'windows' will work. Supported: windows, ios, android, mobile.
      Added support for OS versions, e.g. 'windows[6.1-]'.
      Added support for negative statements, e.g. '!chrome' will return a positive if you're not using Chrome.
      Added support for logical AND & or && (whatever suits you best). 'windows, !android && firefox' means 'using Windows, or Firefox but not in Android.'
    
    ! Fixed ambiguity with browser/OS versions in Wess's prefix handler. $android should now only be set for the Android stock browser, $iphone is replaced with $ios, and $safari should no longer be set in iOS, for the purpose of these tests of course. (Class-CSS.php)
    
    + Non-Android, non-iOS mobile devices will now show a regular wireless icon. (Post2.php)
    
    * French Translation. (Errors, ManageSettings)

--- attic/Unused-dynPrefixed.php
+++ attic/Unused-dynPrefixed.php
@@ -3,25 +3,29 @@
 	This is a rewrite of a Subs-Cache.php function to add CSS prefixes only when needed.
 	I didn't use it in the end, because it would have forced us to create one cache file for every
 	single browser version, instead of just the one.
+
+	Okay, interestingly, I DID end up doing a cache file for every version, but without using
+	this piece of code, which I had forgotten about... And while it's more readable, it's also less flexible
+	because it only allows for boolean prefixed/unprefixed, instead of allowing to completely remove a rule.
 */
 
 // Numbers taken from http://caniuse.com
 $rule_list = array(
-	'box-sizing'	=> array( 8, -1, 10,  5, 5.1,  9.6, 2.2),
-	'box-shadow'	=> array( 9,  4, 10,  5, 5.1, 10.5,  -1),
-	'border-radius'	=> array( 9,  4,  5,  4,   5, 10.5, 2.2),
-	'transition'	=> array(-1, -1, -1, -1,  -1,   -1,  -1),
+	// Effect				  IE Firefox Chrome Safari  iOS Opera Android Generic
+	'box-sizing'	=> array(  8,     -1,    10,     5, 5.1,  9.6,    2.2,     -1 ),
+	'box-shadow'	=> array(  9,      4,    10,     5, 5.1, 10.5,     -1,     -1 ),
+	'border-radius'	=> array(  9,      4,     5,     4,   5, 10.5,    2.2,     -1 ),
+	'transition'	=> array( -1,     -1,    -1,    -1,  -1,   -1,     -1,     -1 ),
 );
 
-// And now let's magically transform those CSS3 rules that are prominent enough in Wedge get the honor of a custom function.
+// And now let's magically transform those CSS3 rules that are prominent enough in Wedge to get the honor of a custom function.
 $final = preg_replace_callback('~(?<!-)(?:' . implode('|', array_keys($rule_list)) . '):[^\n;]+[\n;]~', 'wedge_fix_browser_css', $final);
 
 function wedge_fix_browser_css($matches)
 {
 	global $browser, $prefix, $rule_list;
 
-	$index = $browser['is_ie'] ? 0 : ($browser['is_firefox'] ? 1 : ($browser['is_chrome'] ? 2 : ($browser['is_iphone'] || $browser['is_tablet'] ? 3 :
-			($browser['is_opera'] ? 5 : ($browser['is_android'] ? 6 : 4)))));
+	$index = $browser['is_ie'] ? 0 : ($browser['is_firefox'] ? 1 : ($browser['is_chrome'] ? 2 : ($browser['is_ios'] ? 3 : ($browser['is_safari'] ? 4 : ($browser['is_opera'] ? 5 : ($browser['is_android'] ? 6 : 7))))));
 
 	foreach ($rule_list as $key => $rule)
 		if (strpos($matches[0], $key) === 0)
