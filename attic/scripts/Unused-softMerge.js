c8a86312a6da51de77b73883bf9c2460121c5c95
Nao <nao@wedge>
2013-07-31 15:10:14 +0000

    + Implemented soft-merging of posts. This is similar to regular double-post merging, except it's done at display time, rather than in the database, allowing for internal parent IDs not to get screwed, thus allowing for easier threaded mode, in case we eventually implement this. It's also done automatically, and is quite fast (1ms to merge all posts in a page together), the only drawback really is that it makes the msg skeleton structure a bit less flexible, but whatever... Also turned a few msg blocks into macros, yay. (Subs-Template.php, Msg.template.php, macros.xml, sections.css)
    + Added, for reference, the JavaScript prototype of soft-merging, to the attic. (attic/scripts/Unused-softMerge.js)
    
    + Macros can now use extra params even without specifying an <if> tag in their body. I should have dealt with that long ago, but, well, I didn't even notice the problem was there... (Subs-Template.php)
    
    * Chrome 29+ should support min-resolution CSS stuff now, so I'm changing the logic to save a few bytes.

---
+++ attic/scripts/Unused-softMerge.js
@@ -0,0 +1,29 @@
+/*
+	Before I wrote the PHP code to soft-merge posts (see Subs-Template.php),
+	I wrote a prototype in JavaScript, which worked well enough to gather interest,
+	but had at least one major issue for me: message anchors didn't work properly,
+	at least when targeting an element that would be merged into another.
+	The PHP rewrite fixes this, but I like the JS version better, so I'm including
+	it here, if only for nostalgia of some code that only lived for a few days in Wedge...
+*/
+
+var post, ex_post, $area, $ex_area, cur_bg;
+$('#forumposts .root').not('.postheader').each(function ()
+{
+	post = $(this).find('.umme').data('id');
+	$area = $(this).find('.postarea');
+	if (ex_post == post)
+	{
+		$ex_area.find('.signature').remove();
+		$('<div/>').addClass('merged root').attr('id', $(this).attr('id')).append($area.contents()).appendTo($ex_area);
+		$(this).prev('hr').andSelf().remove();
+	}
+	else
+	{
+		ex_post = post;
+		$ex_area = $area;
+		if (cur_bg)
+			$(this).removeClass(cur_bg).addClass(cur_bg == 'postbg2' ? 'postbg' : 'postbg2');
+		cur_bg = $(this).hasClass('postbg') ? 'postbg' : 'postbg2';
+	}
+});
