003f635d035a808f32ae6fd060965148946a46df
Nao <nao@wedge>
2013-09-13 11:24:04 +0000

    + The 'zoom' link on Zoomedia popups now behaves like a double-click on the image itself, i.e. it opens it on the same page. (zoomedia.js)
    + IE6 will now properly support all min-height rules in CSS files. It's actually very helpful, from what I can see. (Class-CSS.php)
    
    * I've finally found the 'correct' way to properly align background-integrated icons with the text they're shown next to (as long as said text is not too small.) I don't see myself doing it on every single icon though, but I'll at least give it a try for feed icons. (Boards.template.php, Media.template.php, Memberlist.template.php, attic/Custom.template.php, sections.css, Wine/extra.css)
    
    * Added flexbox layout to media item pages. Although it looks better than before ('on supporting browsers'), I'm starting to think that this layout is getting old, and should be replaced with something simpler, with just some floated elements. (Media.template.php, media.css)

--- attic/Custom.template.php
+++ attic/Custom.template.php
@@ -6,6 +6,27 @@
 	most useful for modifying a function in the index template without
 	having to create a new theme based on the current one.
 
+	Another way to override a function would be to add a custom.xml file
+	to your desired skin, and add this code:
+
+	<template name="template_sidebar_feed">
+		global $txt;
+
+		echo '
+	<section>
+		<we:title>
+			<div class="feed_icon">', $txt['feed'], '</div>
+		</we:title>
+		<p>
+			<em>...SPOILERS!</em>
+		</p>
+	</section>';
+	</template>
+
+	The custom.xml technique is more flexible, as it allows to only
+	override the function in some situations, but you're losing
+	the benefit of PHP-specific syntax highlighting in your editor.
+
 	Seriously, you have no idea how much some people hate doing theme work.
 	(And yes, that includes me. -- Nao)
 */
@@ -15,10 +36,12 @@ function template_sidebar_feed_override()
 	global $txt;
 
 	echo '
+	<section>
 		<we:title>
-			<div class="feed_icon"></div>
-			', $txt['feed'], '
+			<div class="feed_icon">', $txt['feed'], '</div>
 		</we:title>
-		<br>
-		<em>...SPOILERS!</em>';
+		<p>
+			<em>...SPOILERS!</em>
+		</p>
+	</section>';
 }
